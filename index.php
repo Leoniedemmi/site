d0737efc41bc70929282e5cecc5180a345e49feb
Hugo <queninhugopro@gmail.com>
2025-01-15 16:36:32 +0100

    Carroussel + images + mini changements

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!-- filepath: /Applications/MAMP/htdocs/site/index.php -->
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -8,13 +9,7 @@
     <?php include 'ariane.php'; $current_page = basename($_SERVER['PHP_SELF']); ?>
     <link rel="stylesheet" href="css/ariane.css">
 </head>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Header avec Logo, Titre et Panier</title>
-</head>
 <body>
-
     <header>
         <!-- Logo -->
         <div class="logo">
@@ -34,64 +29,49 @@
         </div>
     </header>
 
-</body>
-<body>
     <section class="header">
-    <header>
-        <nav>
-            <ul>
-            <section class="mot">
-                <li><a href="randonner.php">ski randonner</a></li>
-                <li><a href="piste.php">ski de piste</a></li>
-                <li><a href="fond.php">ski de fond</a></li>
-                <li><a href="freestyle.php">ski freestyle</a></li>
-                <li><a href="snowboard.php">snowboard</a></li>
-            </ul>
-        </nav>
-    </header>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="randonner.php">ski randonner</a></li>
+                    <li><a href="piste.php">ski de piste</a></li>
+                    <li><a href="fond.php">ski de fond</a></li>
+                    <li><a href="freestyle.php">ski freestyle</a></li>
+                    <li><a href="snowboard.php">snowboard</a></li>
+                </ul>
+            </nav>
+        </header>
     </section>
+
     <div class="container">
-            <?php echo generate_breadcrumb(); ?>
-            <p>Vous êtes sur la page <?php echo ucfirst(str_replace('_', ' ', pathinfo($current_page, PATHINFO_FILENAME))); ?></p>
+        <?php echo generate_breadcrumb(); ?>
+        <p>Vous êtes sur la page <?php echo ucfirst(str_replace('_', ' ', pathinfo($current_page, PATHINFO_FILENAME))); ?></p>
     </div>
+
     <main>
         <section class="features">
             <link rel="stylesheet" href="css/produit.css">
-            <h2>Nos produits</h2>
-            <div class="product-card">
-                <img src="img/ski1.jpg" alt="Ski 1">
-                <h3>Ski X</h3>
-                <p>Prix : 299€</p>
-                <a href="randonner.php">Voir plus</a>
-            </div>
-            <div class="product-card">
-                <img src="img/ski2.jpg" alt="Ski 2">
-                <h3>Ski Y</h3>
-                <p>Prix : 399€</p>
-                <a href="piste.php">Voir plus</a>
-            </div>
-            <div class="product-card">
-                <img src="img/ski1.jpg" alt="Ski 1">
-                <h3>Ski X</h3>
-                <p>Prix : 299€</p>
-                <a href="fond.php">Voir plus</a>
-            </div>
-            <div class="product-card">
-                <img src="img/ski2.jpg" alt="Ski 2">
-                <h3>Ski Y</h3>
-                <p>Prix : 399€</p>
-                <a href="freestyle.php">Voir plus</a>
-            </div>
-            <div class="product-card">
-                <img src="img/ski1.jpg" alt="Ski 1">
-                <h3>Ski X</h3>
-                <p>Prix : 299€</p>
-                <a href="snowboard.php">Voir plus</a>
+            <div class="carousel">
+                <div class="carousel-inner">
+                    <div class="product-card">
+                        <img src="img/ski_de_fond/ATOMIC_PRO_239,99.png" alt="Ski 1">
+                    </div>
+                    <div class="product-card">
+                        <img src="img/ski_de_piste/HEAD_SHAPE_324,90.png" alt="Ski 2">
+                    </div>
+                    <div class="product-card">
+                        <img src="img/ski_de_rando/VOLKL_RISEUP_384,90.png" alt="Ski 1">
+                    </div>
+                    <div class="product-card">
+                        <img src="img/ski_freestyle/ARV_84_253,90.png" alt="Ski 2">
+                    </div>
+                    <div class="product-card">
+                        <img src="img/snowboard/JONES_WEAVER_543,80.png" alt="Ski 2">
+                </div>
             </div>
         </section>
     </main>
-    <footer>
-        <p>&copy; 2024 Magasin de Ski</p>
-    </footer>
+
+    <script src="js/carousel.js"></script>
 </body>
 </html>
